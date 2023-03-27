e157fb06958e71cd7f1bb8a6de320aa2d1558598
askl315 <askl315@gmail.com>
2023-03-26 22:44:29 -0400

    uploaded file

---
+++ TicTacToe/TicTacToe/YichenYLLiuAssignment1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace YichenYLLiuAssignment1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pos2 = new System.Windows.Forms.PictureBox();
+            this.pos3 = new System.Windows.Forms.PictureBox();
+            this.pos4 = new System.Windows.Forms.PictureBox();
+            this.pos5 = new System.Windows.Forms.PictureBox();
+            this.pos6 = new System.Windows.Forms.PictureBox();
+            this.pos8 = new System.Windows.Forms.PictureBox();
+            this.pos9 = new System.Windows.Forms.PictureBox();
+            this.pos7 = new System.Windows.Forms.PictureBox();
+            this.pos1 = new System.Windows.Forms.PictureBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pos2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pos3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pos4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pos5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pos6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pos8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pos9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pos7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pos1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pos2
+            // 
+            this.pos2.BackColor = System.Drawing.Color.DimGray;
+            this.pos2.Location = new System.Drawing.Point(128, 33);
+            this.pos2.Name = "pos2";
+            this.pos2.Size = new System.Drawing.Size(86, 83);
+            this.pos2.TabIndex = 8;
+            this.pos2.TabStop = false;
+            this.pos2.Click += new System.EventHandler(this.pos2_Click);
+            // 
+            // pos3
+            // 
+            this.pos3.BackColor = System.Drawing.Color.DimGray;
+            this.pos3.Location = new System.Drawing.Point(220, 33);
+            this.pos3.Name = "pos3";
+            this.pos3.Size = new System.Drawing.Size(86, 83);
+            this.pos3.TabIndex = 7;
+            this.pos3.TabStop = false;
+            this.pos3.Click += new System.EventHandler(this.pos3_Click);
+            // 
+            // pos4
+            // 
+            this.pos4.BackColor = System.Drawing.Color.DimGray;
+            this.pos4.Location = new System.Drawing.Point(36, 122);
+            this.pos4.Name = "pos4";
+            this.pos4.Size = new System.Drawing.Size(86, 83);
+            this.pos4.TabIndex = 6;
+            this.pos4.TabStop = false;
+            this.pos4.Click += new System.EventHandler(this.pos4_Click);
+            // 
+            // pos5
+            // 
+            this.pos5.BackColor = System.Drawing.Color.DimGray;
+            this.pos5.Location = new System.Drawing.Point(128, 122);
+            this.pos5.Name = "pos5";
+            this.pos5.Size = new System.Drawing.Size(86, 83);
+            this.pos5.TabIndex = 5;
+            this.pos5.TabStop = false;
+            this.pos5.Click += new System.EventHandler(this.pos5_Click);
+            // 
+            // pos6
+            // 
+            this.pos6.BackColor = System.Drawing.Color.DimGray;
+            this.pos6.Location = new System.Drawing.Point(220, 122);
+            this.pos6.Name = "pos6";
+            this.pos6.Size = new System.Drawing.Size(86, 83);
+            this.pos6.TabIndex = 4;
+            this.pos6.TabStop = false;
+            this.pos6.Click += new System.EventHandler(this.pos6_Click);
+            // 
+            // pos8
+            // 
+            this.pos8.BackColor = System.Drawing.Color.DimGray;
+            this.pos8.Location = new System.Drawing.Point(128, 211);
+            this.pos8.Name = "pos8";
+            this.pos8.Size = new System.Drawing.Size(86, 83);
+            this.pos8.TabIndex = 3;
+            this.pos8.TabStop = false;
+            this.pos8.Click += new System.EventHandler(this.pos8_Click);
+            // 
+            // pos9
+            // 
+            this.pos9.BackColor = System.Drawing.Color.DimGray;
+            this.pos9.Location = new System.Drawing.Point(220, 211);
+            this.pos9.Name = "pos9";
+            this.pos9.Size = new System.Drawing.Size(86, 83);
+            this.pos9.TabIndex = 2;
+            this.pos9.TabStop = false;
+            this.pos9.Click += new System.EventHandler(this.pos9_Click);
+            // 
+            // pos7
+            // 
+            this.pos7.BackColor = System.Drawing.Color.DimGray;
+            this.pos7.Location = new System.Drawing.Point(36, 211);
+            this.pos7.Name = "pos7";
+            this.pos7.Size = new System.Drawing.Size(86, 83);
+            this.pos7.TabIndex = 1;
+            this.pos7.TabStop = false;
+            this.pos7.Click += new System.EventHandler(this.pos7_Click);
+            // 
+            // pos1
+            // 
+            this.pos1.BackColor = System.Drawing.Color.DimGray;
+            this.pos1.Location = new System.Drawing.Point(36, 33);
+            this.pos1.Name = "pos1";
+            this.pos1.Size = new System.Drawing.Size(86, 83);
+            this.pos1.TabIndex = 0;
+            this.pos1.TabStop = false;
+            this.pos1.Click += new System.EventHandler(this.pos1_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(128, 307);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 9;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(325, 342);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.pos2);
+            this.Controls.Add(this.pos3);
+            this.Controls.Add(this.pos4);
+            this.Controls.Add(this.pos5);
+            this.Controls.Add(this.pos6);
+            this.Controls.Add(this.pos8);
+            this.Controls.Add(this.pos9);
+            this.Controls.Add(this.pos7);
+            this.Controls.Add(this.pos1);
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe";
+            ((System.ComponentModel.ISupportInitialize)(this.pos2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pos3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pos4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pos5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pos6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pos8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pos9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pos7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pos1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pos1;
+        private System.Windows.Forms.PictureBox pos7;
+        private System.Windows.Forms.PictureBox pos9;
+        private System.Windows.Forms.PictureBox pos8;
+        private System.Windows.Forms.PictureBox pos6;
+        private System.Windows.Forms.PictureBox pos5;
+        private System.Windows.Forms.PictureBox pos4;
+        private System.Windows.Forms.PictureBox pos3;
+        private System.Windows.Forms.PictureBox pos2;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ TicTacToe/TicTacToe/YichenYLLiuAssignment1.cs
@@ -0,0 +1,229 @@
+﻿/* Tic Tac Toe.cs
+ * Assignment 1
+ * 
+ *  Revision History:
+ *         Yichen Liu, 2020.09.20:Created
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YichenYLLiuAssignment1
+
+{
+    public partial class Form1 : Form
+    {
+
+        // variables to keep track of current game state
+        bool isX = true;
+        int turn = 0;
+        char[] positionValues = new char[10] { 'n', 'n', 'n', 'n', 'n', 'n', 'n', 'n', 'n', 'n'};
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Handle the event when click on a PictureBox
+        /// </summary>
+        /// <param name="position">the clicked PictureBox</param>
+        /// <param name="index">the related index of clicked PictureBox (eg. 1 for pos1, 9 for pos9)</param>
+        private void click (PictureBox position, int index)
+        {
+            // when the clicked PictureBox is null, change the image to X or O depend on current turn.
+            // then update positionValues to keep track of current game board.
+            if (position.Image == null)
+            {
+                if (isX)
+                {
+                    position.Image = TicTacToe.Properties.Resources.X;
+                    isX = false;
+                    turn += 1;
+                    positionValues[index] = 'x';
+                }
+                else if (!isX)
+                {
+                    position.Image = TicTacToe.Properties.Resources.O;
+                    isX = true;
+                    turn += 1;
+                    positionValues[index] = 'o';
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// reset the game board to initial state
+        /// </summary>
+        private void reset ()
+        {
+            pos1.Image = null;
+            pos2.Image = null;
+            pos3.Image = null;
+            pos4.Image = null;
+            pos5.Image = null;
+            pos6.Image = null;
+            pos7.Image = null;
+            pos8.Image = null;
+            pos9.Image = null;
+            turn = 0;
+            isX = true;
+            for (int i =0;i<positionValues.Length; i++)
+            {
+                positionValues[i] = 'n';
+            }    
+        }
+
+
+        /// <summary>
+        /// check if there is a winner, return related character
+        /// </summary>
+        /// <returns>'n' for no winner, 'd' for draw, 'x' for X is winner, 'o' for O is winner</returns>
+        private char checkWin()
+        {
+            if (positionValues[1]==positionValues[2] && positionValues[2]==positionValues[3]&& positionValues[1]!='n')
+            {
+                return positionValues[1];
+            }
+            else if (positionValues[1] == positionValues[5] && positionValues[5] == positionValues[9] && positionValues[1] != 'n')
+            {
+                return positionValues[1];
+            }
+            else if (positionValues[1] == positionValues[4] && positionValues[4] == positionValues[7] && positionValues[1] != 'n')
+            {
+                return positionValues[1];
+            }
+            else if (positionValues[4] == positionValues[5] && positionValues[4] == positionValues[6] && positionValues[4] != 'n')
+            {
+                return positionValues[4];
+            }
+            else if (positionValues[7] == positionValues[8] && positionValues[8] == positionValues[9] && positionValues[7] != 'n')
+            {
+                return positionValues[8];
+            }
+            else if (positionValues[2] == positionValues[8] && positionValues[5] == positionValues[8] && positionValues[2] != 'n')
+            {
+                return positionValues[2];
+            }
+            else if (positionValues[3] == positionValues[6] && positionValues[6] == positionValues[9] && positionValues[3] != 'n')
+            {
+                return positionValues[3];
+            }
+            else if (positionValues[3] == positionValues[5] && positionValues[5] == positionValues[7] && positionValues[3] != 'n')
+            {
+                return positionValues[3];
+            }
+
+
+            if (turn == 9)
+            {
+                return 'd';
+            }    
+            return 'n';
+        }
+
+        /// <summary>
+        /// pop up winning message box and reset the board when game ends
+        /// </summary>
+        /// <param name="winner">character that represent who the winner is</param>
+        private void showWinner (char winner)
+        {
+            if (winner == 'x')
+            {
+                MessageBox.Show("X player won the game!");
+                reset();
+            }
+            else if (winner == 'o')
+            {
+                MessageBox.Show("O player won the game!");
+                reset();
+            }
+            else if (winner == 'd')
+            {
+                MessageBox.Show("Game draw!");
+                reset();
+            }
+        }
+
+
+
+
+        private void pos1_Click(object sender, EventArgs e)
+        {
+            click(pos1,1);
+            showWinner(checkWin());
+        }
+
+        private void pos2_Click(object sender, EventArgs e)
+        {
+            click(pos2,2);
+
+            showWinner(checkWin());
+        }
+
+        private void pos3_Click(object sender, EventArgs e)
+        {
+            click(pos3,3);
+
+            showWinner(checkWin());
+        }
+
+        private void pos4_Click(object sender, EventArgs e)
+        {
+            click(pos4,4);
+
+            showWinner(checkWin());
+        }
+
+        private void pos5_Click(object sender, EventArgs e)
+        {
+            click(pos5,5);
+
+            showWinner(checkWin());
+        }
+
+        private void pos6_Click(object sender, EventArgs e)
+        {
+            click(pos6,6);
+
+            showWinner(checkWin());
+        }
+
+        private void pos7_Click(object sender, EventArgs e)
+        {
+            click(pos7,7);
+
+            showWinner(checkWin());
+        }
+
+        private void pos8_Click(object sender, EventArgs e)
+        {
+            click(pos8,8);
+
+            showWinner(checkWin());
+        }
+
+        private void pos9_Click(object sender, EventArgs e)
+        {
+            click(pos9,9);
+
+            showWinner(checkWin());
+        }
+
+        // manual reset button
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            reset();
+        }
+    }
+}
